5fef6545342ba46ad5c8087242ae78eae1b6d215
Mon Nom d'utilisateur <nmarrocchi@la-providence.net>
2021-03-12 15:35:48 +0100

    Add Background, Font menu & Transition Open/close

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/png" href="img/Icon.png">
+        <link type="text/css" rel="stylesheet" href="style/style.css">
+        <link type="text/css" rel="stylesheet" href="style/playerStats.css">
+        <script type="text/javascript" src="functions.js"></script>
+        <title>MH Simulator - Accueil</title>
+    </head>
+
+    <body>
+
+    <?php include "menu.php" ?>
+
+    </body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,13 @@
+<div id="PlayerStats" class="open">
+
+    <p><span id="Username">???</span></p>
+    <p>----------</p>
+    <p>Level <span id="Level">11</span></p>
+    <p>----------</p>
+    <p>Exp    : <span id="Exp">734</span> / <span id="expmax">999</span></p>
+    <p>----------</p>
+    <p>Money     : <span id="Money">3858</span></p>
+
+</div>
+
+    <input id="PlayerStatsButton" type="button" onclick="ChangeValuePSB('PlayerStatsButton')" value=">">
