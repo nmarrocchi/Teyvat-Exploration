d1161f1a4548ebb7f37478f1accd854ad23a5c9b
Mon Nom d'utilisateur <nmarrocchi@la-providence.net>
2021-03-18 08:56:14 +0100

    Add login/register pages

---
+++ login.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/png" href="img/Klee_Icon.png">
+        <link type="text/css" rel="stylesheet" href="style/style.css">
+        <link type="text/css" rel="stylesheet" href="style/playerStats.css">
+        <script type="text/javascript" src="functions.js"></script>
+        <title>Teyvat Exploration - login</title>
+    </head>
+
+    <body>
+
+    <?php include "menu.php" ?>
+
+    </body>
+
+</html>
